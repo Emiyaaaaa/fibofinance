8fdf5df7615f73a24d517dc90655504f962d68ab
emiyaaaaa <emiya@emiya.com.cn>
2025-04-01 18:05:49 +0800

    feat: read language from header

--- utils/i18n/locale.ts
+++ utils/i18n/locale.ts
@@ -1,16 +1,27 @@
 "use server";
 
-import { cookies } from "next/headers";
+import { cookies, headers } from "next/headers";
 
-import { Locale, defaultLocale } from "./config";
+import { Locale, defaultLocale, locales } from "./config";
 
 const COOKIE_NAME = "NEXT_LOCALE";
 
 export async function getUserLocale(): Promise<Locale> {
-  return (
-    ((await cookies()).get(COOKIE_NAME)?.value.split("-")[0] as Locale) ||
-    defaultLocale
-  );
+  const [localFromCookie, localFromHeader] = await Promise.all([
+    (await cookies()).get(COOKIE_NAME)?.value.split("-")[0] as Locale,
+    (await headers())
+      .get("accept-language")
+      ?.split(",")[0]
+      .split("-")[0] as Locale,
+  ]);
+
+  const local = localFromCookie || localFromHeader || defaultLocale;
+
+  if (!locales.includes(local)) {
+    return defaultLocale;
+  }
+
+  return local;
 }
 
 export async function setUserLocale(locale: Locale) {
